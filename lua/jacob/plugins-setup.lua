be7c4af2890162b3fc91dd284e22cfaf6fd0598e
Jacob Wert <jacob.wert1@gmail.com>
2024-01-16 17:48:31 -0800

    Finished Josean's video for initial setup of neovim.

--- lua/jacob/plugins-setup.lua
+++ lua/jacob/plugins-setup.lua
@@ -1,12 +1,12 @@
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
 end
 
 local packer_bootstrap = ensure_packer()
@@ -21,75 +21,90 @@ vim.cmd([[
 
 local status, packer = pcall(require, "packer")
 if not status then
-    return
+	return
 end
 
 -- Installing packages
-require('packer').startup(function(use)
-    use {"wbthomason/packer.nvim"}
+require("packer").startup(function(use)
+	use({ "wbthomason/packer.nvim" })
 
-    -- lua functions that many plugins use
-    use {"nvim-lua/plenary.nvim"}
+	-- lua functions that many plugins use
+	use({ "nvim-lua/plenary.nvim" })
 
-    -- theme
-    use {"ellisonleao/gruvbox.nvim"}
+	-- theme
+	use({ "ellisonleao/gruvbox.nvim" })
 
-    -- tmux & split window navigation
-    use {"christoomey/vim-tmux-navigator"}
-    use {"szw/vim-maximizer"}
+	-- tmux & split window navigation
+	use({ "christoomey/vim-tmux-navigator" })
+	use({ "szw/vim-maximizer" })
 
-    -- essential plugins
-    use {"tpope/vim-surround"}
-    use {"vim-scripts/ReplaceWithRegister"}
+	-- essential plugins
+	use({ "tpope/vim-surround" })
+	use({ "vim-scripts/ReplaceWithRegister" })
 
-    -- commenting with gc
-    use {"numToStr/Comment.nvim"}
+	-- commenting with gc
+	use({ "numToStr/Comment.nvim" })
 
-    -- file explorer
-    use {"nvim-tree/nvim-tree.lua"}
+	-- file explorer
+	use({ "nvim-tree/nvim-tree.lua" })
 
-    -- icons
-    use {"kyazdani42/nvim-web-devicons"}
+	-- icons
+	use({ "kyazdani42/nvim-web-devicons" })
 
-    -- statusline
-    use {"nvim-lualine/lualine.nvim"}
+	-- statusline
+	use({ "nvim-lualine/lualine.nvim" })
 
-    -- fuzzy finding
-    use({"nvim-telescope/telescope-fzf-native.nvim", run = "make"}) -- dependency
-    use({"nvim-telescope/telescope.nvim", branch = "0.1.x"})
+	-- fuzzy finding
+	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency
+	use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" })
 
-    -- autocompletion
-    use{ "hrsh7th/nvim-cmp" } -- used for autocompletion
-    use{ "hrsh7th/cmp-buffer" } -- used to let nvim-cmp recommend things from current buffer
-    use{ "hrsh7th/cmp-path" } -- used to let nvim-cmp recomend things when we create files
+	-- autocompletion
+	use({ "hrsh7th/nvim-cmp" }) -- used for autocompletion
+	use({ "hrsh7th/cmp-buffer" }) -- used to let nvim-cmp recommend things from current buffer
+	use({ "hrsh7th/cmp-path" }) -- used to let nvim-cmp recomend things when we create files
 
-    -- snippets
-    use({ "L3MON4D3/LuaSnip",
-	    -- follow latest release.
-	    tag = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-	    -- install jsregexp (optional!:).
-	    run = "make install_jsregexp"
-    })
-    use {"saadparwaiz1/cmp_luasnip"}
-    use {"rafamadriz/friendly-snippets"}
+	-- snippets
+	use({
+		"L3MON4D3/LuaSnip",
+		-- follow latest release.
+		tag = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+		-- install jsregexp (optional!:).
+		run = "make install_jsregexp",
+	})
+	use({ "saadparwaiz1/cmp_luasnip" })
+	use({ "rafamadriz/friendly-snippets" })
 
-    -- managing and installing lsp servers, linters and formatters
-    use {"williamboman/mason.nvim"}
-    use {"williamboman/mason-lspconfig.nvim"}
+	-- managing and installing lsp servers, linters and formatters
+	use({ "williamboman/mason.nvim" })
+	use({ "williamboman/mason-lspconfig.nvim" })
 
-    -- configuring lsp servers
-    use {"neovim/nvim-lspconfig"}
-    use {"hrsh7th/cmp-nvim-lsp"}
-    use ({"glepnir/lspsaga.nvim", branch = "main" })
-    use {"jose-elias-alvarez/typescript.nvim"}
-    use {"onsails/lspkind.nvim"}
+	-- configuring lsp servers
+	use({ "neovim/nvim-lspconfig" })
+	use({ "hrsh7th/cmp-nvim-lsp" })
+	use({ "glepnir/lspsaga.nvim", branch = "main" })
+	use({ "jose-elias-alvarez/typescript.nvim" })
+	use({ "onsails/lspkind.nvim" })
 
-    -- formatting and linting
-    use {"jose-elias-alvarez/null-ls.nvim"}
-    use {"jayp0521/mason-null-ls.nvim"}
+	-- formatting and linting
+	use({ "jose-elias-alvarez/null-ls.nvim" })
+	use({ "jayp0521/mason-null-ls.nvim" })
 
-    if packer_boostrap then
-        require("packer").sync()
-    end
+	-- treesitter
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = function()
+			require("nvim-treesitter.install").update({ with_sync = true })
+		end,
+	})
 
+	-- auto closing
+	use({ "windwp/nvim-autopairs" })
+	use({ "windwp/nvim-ts-autotag" })
+
+	-- git signs plugin
+	use({ "lewis6991/gitsigns.nvim" })
+
+	if packer_boostrap then
+		require("packer").sync()
+	end
 end)
